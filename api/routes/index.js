8067bff9a486d2847e50da102e3b68afdbb8a778
Marin ROUX <marin_maxfresh-and-good@hotmail.fr>
2020-08-21 20:31:43 +0000

    Change api endpoint

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,7 +2,7 @@ const system = require('./../system')
 const medicaments = require('./../medicaments')
 
 exports.configure = function (app, options) {
-  app.use('/api', system(options));
-  app.use('/api/medicaments', medicaments(options));
+  app.use('/', system(options));
+  app.use('/medicaments', medicaments(options));
 
 };
